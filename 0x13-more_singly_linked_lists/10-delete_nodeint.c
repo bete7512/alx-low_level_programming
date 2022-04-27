e61c1a9cdd3479cefe9049f7de89a43c726c4632
bete7512 <betekbebe@gmail.com>
2022-04-27 10:58:44 -0700

    amazing

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,6 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			temp = holder->next;
 			holder->next = temp->next;
+			free(temp);
 			return (1);
 		}
 		holder = holder->next;
