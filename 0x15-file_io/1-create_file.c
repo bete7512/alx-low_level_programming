f77fb06aa4ae2e511c728748ca8f7baaa62931c2
bete7512 <betekbebe@gmail.com>
2022-05-03 12:58:22 -0700

    2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+
+
+size_t _strlen(char *s)
+{
+	size_t i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, close_check;
+	size_t length;
+	ssize_t write_actual;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	length = _strlen(text_content);
+
+	write_actual = write(fd, text_content, length);
+	if (write_actual == -1)
+		return (-1);
+
+	close_check = close(fd);
+	if (close_check == -1)
+		return (-1);
+
+	return (1);
+}
