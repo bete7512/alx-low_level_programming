645a3c02d17d3961f2051b69cbf836b1400e57ab
bete7512 <betekbebe@gmail.com>
2022-05-03 13:03:05 -0700

    fucking country we have

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+
+size_t _strlen(char *s)
+{
+	size_t i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, close_check;
+	size_t length;
+	ssize_t write_actual;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_RDWR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	length = _strlen(text_content);
+
+	write_actual = write(fd, text_content, length);
+
+	if (write_actual == -1)
+		return (-1);
+
+	close_check = close(fd);
+
+	if (close_check == -1)
+		return (-1);
+
+	return (1);
+}
+
