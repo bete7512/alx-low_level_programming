e59c41dc66f690fa991159f3912e1ea1614713e8
bete7512 <betekbebe@gmail.com>
2022-06-09 23:50:28 -0700

    doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - print and return number of data in a linked list
+ *@h:pointers passed to our function
+ *Return: type size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t k = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		k++;
+		h = h->next;
+	}
+	return (k);
+}
