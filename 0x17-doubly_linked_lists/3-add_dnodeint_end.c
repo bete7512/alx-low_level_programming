6916de49561c5a193e92097ffacaf80008449c05
bete7512 <betekbebe@gmail.com>
2022-06-10 06:56:20 -0700

    heloo

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,11 +12,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (!head)
 		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
 
@@ -26,12 +24,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
-
 	while (temp->next)
 		temp = temp->next;
-
 	temp->next = new;
 	new->prev = temp;
-
 	return (new);
 }
