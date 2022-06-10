32e9e7e3aab39e4dcba9ea4968f84c158a6a93d8
bete7512 <betekbebe@gmail.com>
2022-06-10 00:09:41 -0700

    here

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t k = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		k++;
 		h = h->next;
+		k++;
 	}
 	return (k);
 }
